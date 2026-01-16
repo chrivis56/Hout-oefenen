d2270dca22fc3dcbbd40e3e4a1b577aabb4573f9
chrivis56 <c.roukema@outlook.com>
2026-01-16 11:42:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("NIEUWE DECK-VERSIE ACTIEF1");
+console.log("NIEUWE DECK-VERSIE ACTIEF");
 const woods = [
   { name: "Beuken", image: "images/beuken/1.jpg" },
   { name: "Esdoorn", image: "images/esdoorn/1.jpg" },
